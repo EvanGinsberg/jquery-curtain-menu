4eb81f06494ca58aae5788ea7f8580a9bf724061
Jordan Lewis <jordan@involved.com.au>
2011-09-15 17:12:26 +1000

    HAve defaults overrides working

--- jquery-curtain-menu-1.0.0.js
+++ jquery-curtain-menu-1.0.0.js
@@ -1,62 +1,76 @@
-//
-// create closure
-//
-(function($) {
-  //
-  // plugin definition
-  //
-  $.fn.curtainMenu = function(options) {
-    debug(this);
-   // build main options before element iteration
-   var opts = $.extend({}, $.fn.hilight.defaults, options);
- // iterate and reformat each matched element
- return this.each(function() {
-   $this = $(this);
-   // build element specific options
-   var o = $.meta ? $.extend({}, opts, $this.data()) : opts;
-   // update element styles
-   $this.css({
-  backgroundColor: o.background,
-  color: o.foreground
-   });
-   var markup = $this.html();
-   // call our format function
-   markup = $.fn.hilight.format(markup);
-   $this.html(markup);
- });
-  };
-  //
-  // private function for debugging
-  //
-  function debug($obj) {
-    if (window.console && window.console.log)
-      window.console.log('debug');
-  };
-  //
-  // define and expose our format function
-  //
-  $.fn.curtainMenu.format = function(txt) {
-    return '<strong>' + txt + '</strong>';
-  };
-  //
-  // plugin defaults
-  //
-  $.fn.hilight.defaults = {
-  nav: $('#nav'),
-  navLevel01: $('#nav ul.level01'),
-  navLevel02: $('#nav ul.level02'),
-  curtain: $('#nav .curtain'),
-  //curtain_defaultH: curtain.height(),
-  menuActive: false,
-  menuIntent: false,
-  menuIntentItem: null,
-  revealSpeed: 200,
-  closeSpeed: 500,
-  alterSpeed: 500
-  //subMenu1: $('ul.level02', menuIntentItem)
-};
-//
-// end of closure
-//
-})(jQuery);
+/*!
+* jQuery lightweight plugin boilerplate
+* Original author: @ajpiano
+* Further changes, comments: @addyosmani
+* Licensed under the MIT license
+*/
 
+
+// the semi-colon before function invocation is a safety net against concatenated
+// scripts and/or other plugins which may not be closed properly.
+;(function ( $, window, document, undefined ) {
+    
+    // undefined is used here as the undefined global variable in ECMAScript 3 is
+    // mutable (ie. it can be changed by someone else). undefined isn't really being
+    // passed in so we can ensure the value of it is truly undefined. In ES5, undefined
+    // can no longer be modified.
+    
+    // window and document are passed through as local variables rather than globals
+    // as this (slightly) quickens the resolution process and can be more efficiently
+    // minified (especially when both are regularly referenced in your plugin).
+
+    // Create the defaults once
+		var curtain = null;
+		var curtain_defaultH = null;
+		var menuIntentItem = null;
+		
+    var pluginName = 'curtainMenu',
+        defaults = {
+						nav: $('#nav'),
+					  navLevel01: $('#nav ul.level01'),
+					  navLevel02: $('#nav ul.level02'),
+					  curtain: $('#nav .curtain'),
+					  curtain_defaultH: $(curtain).height(),
+					  menuActive: false,
+					  menuIntent: false,
+					  menuIntentItem: null,
+					  revealSpeed: 200,
+					  closeSpeed: 500,
+					  alterSpeed: 500
+					  //subMenu1: $('ul.level02', menuIntentItem)
+        };
+
+    // The actual plugin constructor
+    function Plugin( element, options ) {
+        this.element = element;
+
+        // jQuery has an extend method which merges the contents of two or
+        // more objects, storing the result in the first object. The first object
+        // is generally empty as we don't want to alter the default options for
+        // future instances of the plugin
+        this.options = $.extend( {}, defaults, options) ;
+        
+        this._defaults = defaults;
+        this._name = pluginName;
+        
+        this.init();
+    }
+
+    Plugin.prototype.init = function () {
+        // Place initialization logic here
+        // You already have access to the DOM element and the options via the instance,
+        // e.g., this.element and this.options
+				console.log("init plugin => " + this.options.curtain_defaultH);
+    };
+
+    // A really lightweight plugin wrapper around the constructor,
+    // preventing against multiple instantiations
+    $.fn[pluginName] = function ( options ) {
+        return this.each(function () {
+            if (!$.data(this, 'plugin_' + pluginName)) {
+                $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
+            }
+        });
+    }
+
+})(jQuery, window);
